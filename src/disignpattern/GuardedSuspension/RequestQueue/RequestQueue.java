531b0c5315ae709e9ff738c9f811316bcecb195f
sky <hsf1002@gmail.com>
2018-08-26 17:23:32 +0800

    BlockingQueue

--- src/disignpattern/GuardedSuspension/RequestQueue/RequestQueue.java
+++ src/disignpattern/GuardedSuspension/RequestQueue/RequestQueue.java
@@ -1,17 +1,21 @@
 package disignpattern.GuardedSuspension.RequestQueue;
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class RequestQueue {
-    private final Queue<Request> queue = new LinkedList<Request>();
+    //private final Queue<Request> queue = new LinkedList<Request>();
+    private final BlockingQueue<Request> queue = new LinkedBlockingQueue<>();
 
-    public synchronized Request getRequest()
+    public /*synchronized */Request getRequest()
     {
-        while (queue.peek() == null)
+        Request request = null;
+        //while (queue.peek() == null)
         {
             try {
-                wait();
+                //wait();
+                // take 已经实现了互斥处理
+                request = queue.take();
             }
             catch (InterruptedException e)
             {
@@ -19,12 +23,22 @@ public class RequestQueue {
             }
         }
 
-        return queue.remove();
+        //return queue.remove();
+        return request;
     }
 
-    public synchronized void putRequest(Request request)
+    public /*synchronized */void putRequest(Request request)
     {
-        queue.offer(request);
-        notifyAll();
+        //queue.offer(request);
+        //notifyAll();
+        try
+        {
+            // put 已经实现了互斥处理
+            queue.put(request);
+        }
+        catch (InterruptedException e)
+        {
+            e.printStackTrace();
+        }
     }
 }
