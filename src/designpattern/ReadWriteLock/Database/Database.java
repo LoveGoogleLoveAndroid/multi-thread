ef4201ff42e394a23799f38e4f73f67758a4bb2e
sky <hsf1002@gmail.com>
2018-09-16 09:31:58 +0800

    ReadWriteLock-Database-ReentrantReadWriteLock

--- src/designpattern/ReadWriteLock/Database/Database.java
+++ src/designpattern/ReadWriteLock/Database/Database.java
@@ -2,30 +2,61 @@ package designpattern.ReadWriteLock.Database;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class Database<K, V> {
     // 本身HashMap不是线程安全的，但是Database确是线程安全的
     private final Map<K, V> map = new HashMap<K, V>();
+    private final ReadWriteLock lock = new ReentrantReadWriteLock(true/* fair */);
+    private final Lock readLock = lock.readLock();
+    private final Lock writeLock = lock.writeLock();
 
-    public synchronized void clear()
+    // 20秒内使用 synchronized 可以读取大约455次
+    // 455 retrieve: Bobby before
+    // 20秒内使用 ReentrantReadWriteLock 可以读取大约2543次
+    // 2543 retrieve: Bobby before
+
+    public /*synchronized*/ void clear()
     {
-        verySlowly();
-        map.clear();
+        writeLock.lock();
+        try{
+            verySlowly();
+            map.clear();
+            }
+        finally {
+            writeLock.unlock();
+        }
     }
 
-    public synchronized void assign(K key, V value)
+    public /*synchronized */void assign(K key, V value)
     {
-        verySlowly();
-        map.put(key, value);
-        System.out.println("database assign: key = " + key + ", value = " + value);
+        writeLock.lock();
+        try {
+            verySlowly();
+            map.put(key, value);
+            System.out.println("database assign: key = " + key + ", value = " + value);
+        }
+        finally {
+            writeLock.unlock();
+        }
     }
 
-    public synchronized V retrieve(K key)
+    public /*synchronized*/ V retrieve(K key)
     {
-         slowly();
-         V value = map.get(key);
-         System.out.println("database retrieve: key = " + key + ", value = " + value);
-         return value;
+        V value;
+
+        readLock.lock();
+        try {
+            slowly();
+            value = map.get(key);
+            System.out.println("database retrieve: key = " + key + ", value = " + value);
+        }
+        finally {
+            readLock.unlock();
+        }
+        return value;
     }
 
     private void slowly()
