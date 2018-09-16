185d263a235df1c28837d10d0e99751e7045cf24
sky <hsf1002@gmail.com>
2018-09-16 17:58:18 +0800

    ThreadPerMessage-Host-ExecutorService

--- src/designpattern/ThreadPerMessage/Host/Host.java
+++ src/designpattern/ThreadPerMessage/Host/Host.java
@@ -1,26 +1,38 @@
 package designpattern.ThreadPerMessage.Host;
 
+import java.util.concurrent.Executor;
 import java.util.concurrent.ThreadFactory;
 
 public class Host {
     private final Helper helper = new Helper();
-    private final ThreadFactory factory;
+    /*private final ThreadFactory factory;
 
     public Host(ThreadFactory factory) {
         this.factory = factory;
+    }*/
+    private final Executor executor;
+
+    public Host(Executor executor) {
+        this.executor = executor;
     }
 
     public void request(final int count, final char c)
     {
         System.out.println("Host request begin");
 
-        factory.newThread(new Runnable() {
+        /*factory.newThread(new Runnable() {
               @Override
               public void run() {
                   helper.handle(count, c);
               }
           }
-        ).start();
+        ).start();*/
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                helper.handle(count, c);
+            }
+        });
 
         System.out.println("Host request end");
     }
--- src/designpattern/ThreadPerMessage/Host/Main.java
+++ src/designpattern/ThreadPerMessage/Host/Main.java
@@ -1,5 +1,7 @@
 package designpattern.ThreadPerMessage.Host;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Main {
@@ -7,7 +9,16 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("main begin");
 
-        Host host = new Host(Executors.defaultThreadFactory());
+        //Host host = new Host(Executors.defaultThreadFactory());
+        /*Host host = new Host(new Executor() {
+            @Override
+            public void execute(Runnable command) {
+                new Thread(command).start();
+            }
+        });*/
+        ExecutorService service = Executors.newCachedThreadPool();
+        Host host = new Host(service);
+
         host.request(10, 'A');
         host.request(15, 'B');
         host.request(20, 'C');
