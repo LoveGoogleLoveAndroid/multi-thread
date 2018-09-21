a33a849bc0a9c0e30af1d1847cdcf02d8bf1154a
sky <hsf1002@gmail.com>
2018-09-21 22:35:05 +0800

    Future-Retrieve-AsyncContentImpl

--- src/designpattern/Future/Retriever/Retriever.java
+++ src/designpattern/Future/Retriever/Retriever.java
@@ -4,7 +4,10 @@ public class Retriever {
 
     public static Content retrieve(String urlStr)
     {
+        // Elapsed time = 1824 mill seconds
         //return new SyncContentImpl(urlStr);
+
+        // Elapsed time = 1233 mill seconds
         final AsyncContentImpl future = new AsyncContentImpl();
 
         new Thread(){
