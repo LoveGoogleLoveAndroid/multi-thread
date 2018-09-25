bab0a56885122f19aa0d96b3487ff74bd4ff8534
sky <hsf1002@gmail.com>
2018-09-25 22:32:04 +0800

    Files-FileInputStream1

--- src/File/Files/IOUtils.java
+++ src/File/Files/IOUtils.java
@@ -27,8 +27,34 @@ public class IOUtils {
         in.close();
     }
 
+    private static void printHexByByteArray(String fileName) throws IOException
+    {
+        FileInputStream in = new FileInputStream(fileName);
+        byte[] buf = new byte[1024];
+
+        int bytes = 0;
+        int j = 1;
+
+        while ((bytes = in.read(buf, 0, buf.length)) != -1)
+        {
+            for (int i = 0; i < bytes; i++) {
+                if ((buf[i] & 0xFF) <= 0xF)
+                {
+                    System.out.print("0");
+                }
+                System.out.print(Integer.toHexString(buf[i] & 0xFF) + "  ");
+
+                if (j++ % 10 == 0)
+                {
+                    System.out.println();
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) throws IOException{
         final String FILE_PATH = "/Users/sky/work/java/MultiThread/README.md";
-        printHex(FILE_PATH);
+        //printHex(FILE_PATH);
+        printHexByByteArray(FILE_PATH);
     }
 }
---
+++ src/File/README.md
@@ -0,0 +1,20 @@
+
+
+## 字节流
+1. InputStream、OutputStream抽象了应用程序读写数据的方式
+2. EOF = End，读到-1即文件结尾
+3. 输入流基本方法：
+```
+int b = in.read()； // 读到一个字节无符号填充到Int低8位
+in.read(byte[] buf); 
+in.read(byte[] buf, int start, int size);
+```
+4. 输出流基本方法：
+```
+out.write(int b)； // 写入一个Int的低8位，即byte到输出流
+out.write(byte[] buf); 
+out.write(byte[] buf, int start, int size);
+```
+5. FileInputStream、FileOutputStream实现了在文件上读取数据
+
+## 字符流
